a5895ba65262728c8f9d2a09063b31b00b25aba9
Lina <taraseviciene.lina@gmail.com>
2022-03-23 19:16:46 +0200

    Initial Commit: training

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Task 1
+        int value_int = 20;
+        String value_string = "Java";
+        double value_double = 23.63;
+
+        System.out.println(value_int);
+        System.out.println(value_string);
+        System.out.println(value_double);
+
+        // Task 2
+
+        int value_of_the_data_type = 50;
+        double another_value_of_the_data_type = 30.2;
+        System.out.println("New variable value_double from value int 50 is (another_value_of_the_data_type): " + another_value_of_the_data_type);
+
+        double sum_value = value_of_the_data_type + another_value_of_the_data_type;
+        System.out.println(sum_value);
+    }
+}
